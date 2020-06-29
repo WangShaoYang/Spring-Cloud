8f9c26ceede8b7e08b288fbddf12e407a583e381
WangShaoYang <872452093@qq.com>
2020-06-29 08:35:29 +0800

    Nacos完结

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope // 通过Spring Cloud原生注解@RefreshScope实现配置自动刷新
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
